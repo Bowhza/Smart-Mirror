28834eb8adbd5a112ac8a8f1d965cd4d88a7e312
Bowhza <bbohnat1@nait.ca>
2024-03-15 13:33:06 -0600

    Added router component for the future.

---
+++ Software/client/src/Router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import { useEffect, useState } from 'react';
+
+const Router = () => {
+  const [ip, setIp] = useState('');
+  const hostIP = import.meta.env.VITE_HOST;
+
+  useEffect(() => {
+    fetch(`http://${hostIP}:5174/get_ip`, { mode: 'cors' })
+      .then(res => res.json())
+      .then(data => {
+        setIp(data.ip);
+        console.log(data.ip);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: ip === hostIP ? <App /> : <h1>Not Host Device</h1>,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
