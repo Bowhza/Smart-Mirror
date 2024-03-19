c012e82315a931a9e9af4d40f0bdd558299aa069
Bowhza <bbohnat1@nait.ca>
2024-03-19 12:56:55 -0600

    Started working on weather settings route.

--- Software/client/src/components/Routes/ClientReminders.jsx
+++ Software/client/src/components/Routes/ClientReminders.jsx
@@ -1,7 +1,7 @@
 export default function ClientReminders() {
   return (
     <div>
-      <header className="border-b-2 p-2 border-zinc-600">
+      <header className="border-b-2 p-2 border-zinc-600 ">
         <h1 className="text-4xl font-bold">Reminders</h1>
       </header>
     </div>
--- Software/client/src/components/Routes/ClientWeather.jsx
+++ Software/client/src/components/Routes/ClientWeather.jsx
@@ -4,6 +4,12 @@ export default function ClientWeather() {
       <header className="border-b-2 p-2 border-zinc-600">
         <h1 className="text-4xl font-bold">Weather</h1>
       </header>
+      <div className="p-3">
+        <div className="flex flex-col">
+          <h2 className="text-2xl">Location</h2>
+          <input name="location" type="text" className="bg-zinc-600 flex-grow h-10" />
+        </div>
+      </div>
     </div>
   );
 }
