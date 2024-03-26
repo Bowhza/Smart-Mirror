4982021db9ae77279cb6244a7fd603658bf223bb
zKeewee <klawrence8@nait.ca>
2024-03-26 16:28:49 -0600

    enhanced delete user and set user functionality

--- Software/server/app.py
+++ Software/server/app.py
@@ -17,28 +17,45 @@ def main():
 # Add user to DB
 @app.route("/add_user/<username>", methods=["POST"])
 def add_user(username):
+    # Replace any white space with an empty string
     username = username.replace(" ", "")
-    if not username.isalnum:
-        return jsonify({"message": "Invalid Format! Usernames must be alphanumeric"})
 
-    username = username.strip()
+    # If the username isn't alphanumeric, return an error
+    if username.isalnum() is False:
+        return jsonify({"message": "Invalid Format! Usernames must be alphanumeric"}), 400
+
+    # username = username.strip()
+    # Check to see if the username already exists in the DB
+    # If so, return an error stating so
     if check_username(username) is True:
         return jsonify({"message": "Username Taken!"}), 406
 
+    # Attempt to enter the user into the DB
     try:
+        # Create the new user and add to the DB
         new_user = Users(username)
         db.session.add(new_user)
         db.session.commit()
+
+        users = Users.query.all()
+        if len(users) == 1:
+            set_user(new_user.userID)
+
+        # Return the ok status code and the message
         return jsonify({"message": "User Added"}), 200
+
+    # If a DB error occurs, return an error message
     except SQLException.DatabaseError:
         return jsonify({"message": "User Not Added"}), 400
     
 
 # Deletes the user from the DB
-@app.route("/delete_user/<user_id>")
+@app.route("/delete_user/<user_id>", methods=["DELETE"])
 def delete_user(user_id):
+    global properties
+
     if check_user_exists(user_id) is False:
-        return jsonify({"message": "user doesnt exist"}), 403
+        return jsonify({"message": "User Doesnt Exist"}), 403
 
     if len(Reminders.query.filter_by(userID=user_id).all()) > 0:
         delete_user_reminders(user_id)
@@ -46,40 +63,91 @@ def delete_user(user_id):
         user = Users.query.filter_by(userID=user_id).first()
         db.session.delete(user)
         db.session.commit()
-    except SQLException.DatabaseError as ex:
-        return jsonify({"message": "user could not be deleted!"}), 403
 
+        if properties["defaultUser"] == user.username or properties["defaultUser"] == "":
+            users = Users.query.all()
+            if len(users) >= 1:
+                user = Users.query.first()
+                set_user(user.userID)
+
+            else:
+                properties["defaultUser"] = ""
+
+            # write the new contents to the JSON file
+            with open("properties.json", "w") as file:
+                json.dump(properties, file, indent=2)
+
+    except SQLException.DatabaseError as ex:
+        return jsonify({"message": "User Could Not be Deleted!"}), 403
+
+    return jsonify({"message": "User Deleted from Database!"}), 200
+
+# Clears everything in the DB
 @app.route("/clear_db")
 def clear_db():
+    # Attempt to clear the DB
     try:
+        # Grab all current users in the DB
         users = Users.query.all()
+
+        # Iterate through each user
         for user in users:
+
+            # Grab the user id and delete the user
+            # This will also delete any user reminders tied to the user id
             user_id = user.userID
             delete_user(user_id)
+
+    # If a DB error occurs, return an error message
     except SQLException.DatabaseError as ex:
         return jsonify({"message": "users could not be deleted!"}), 403
 
+    # Return a message stating the DB was cleared
     return jsonify({"message": "Database cleared!"})
 
 
-
 # Get all current users in the DB
-@app.route("/get_users")
+@app.route("/get_users", methods=["GET"])
 def get_users():
+    # Grab all users in the DB
     users = Users.query.all()
-    json_users = [user.to_json() for user in users]
-    if len(users) == 0:
-        return jsonify({"message": "no users found!"}), 404
 
+    # If no users are inside the DB, return a message stating so
+    # if len(users) == 0:
+    #     return jsonify({"message": "no users found!"}), 404
+
+    # Convert the users info into JSON and store them into a list
+    json_users = [user.to_json() for user in users]
+
+    # Return the users JSON info
     return jsonify(json_users), 200
+
+
+# @app.route("/clear_nulls")
+# def clear_nulls():
+#     try:
+#         reminders = Reminders.query.filter_by(userID=None).all()
+#         for reminder in reminders:
+#             db.session.delete(reminder)
+#         db.session.commit()
+#     except SQLException.DatabaseError as ex:
+#         return jsonify({"message": "cannot delete nulls!"})
+#
+#     return jsonify({"message": "nulls removed!"})
 
 
 # Helper function to check if a username is available
 def check_username(username):
+    # Attempt to query the DB
     try:
+        # Grab all the users in the DB and store the lowercase versions in a seperate list
         users = Users.query.all()
         lowercase_users = [user.username.lower() for user in users]
+
+        # return whether the username is in the lowercase username collection
         return username.lower() in lowercase_users
+
+    # If a DB error occurs, return an error message
     except SQLException.DatabaseError as ex:
         return jsonify({"message": "user could not be searched!"}), 403
 
@@ -188,7 +256,7 @@ def update_sensor_settings(sensor):
 # Retrieves all current settings in the properties.json
 @app.route("/get_settings", methods=["GET"])
 def get_settings():
-    return jsonify(properties)
+    return jsonify(properties), 200
 
 
 @app.route("/update_location/<location>")
@@ -213,18 +281,19 @@ def update_location(location):
 
 
 # Update the current user through the web application
-@app.route("/set_user/<username>")
-def set_user(username):
+@app.route("/set_user/<user_id>", methods=["POST"])
+def set_user(user_id):
     global properties
 
-    # If the user doesnt exist in the DB, prompt an errors
-    if check_username(username) is False:
-        return jsonify({"message": "user doesnt exist!"}), 403
+    # # If the user doesn't exist in the DB, prompt an errors
+    if check_user_exists(user_id) is False:
+        return jsonify({"message": "User doesnt exist!"}), 403
 
     # Attempt to query the DB
     try:
         # Grab the specified user and set the defaultUser field to the username
-        user = Users.query.filter_by(username=username).first()
+        user = Users.query.filter_by(userID=user_id).first()
+
         properties["defaultUser"] = user.username
 
         # Write to the JSON file
@@ -232,11 +301,11 @@ def set_user(username):
             json.dump(properties, file, indent=2)
 
         # Return a status message
-        return jsonify({"message": "username updated in config"}), 200
+        return jsonify({"message": "Username Updated in Config!"}), 200
 
     # If an error occurs, prompt an error message
     except SQLException.DatabaseError as ex:
-        return jsonify({"message": "user could not be updated in config file!"}), 403
+        return jsonify({"message": "User could not be Updated in Config File!"}), 403
 
 
 # Websockets
