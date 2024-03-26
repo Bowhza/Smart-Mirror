8518398d09c2900026ed6d097dfa0102e77464ee
Bowhza <bbohnat1@nait.ca>
2024-03-26 10:26:53 -0600

    Add Notification banner component

--- Software/client/src/Routes/ClientUsers.jsx
+++ Software/client/src/Routes/ClientUsers.jsx
@@ -1,35 +1,47 @@
 import Header from '../components/Header';
 import Input from '../components/Input';
+import Notification from '../components/Notification';
 import { useState } from 'react';
 
 export default function ClientUsers() {
   const hostIP = import.meta.env.VITE_HOST;
   const [response, setResponse] = useState({});
+  const [showBanner, setShowBanner] = useState(false);
 
   const AddUser = Username => {
     if (Username.trim().length > 0) {
       fetch(`http://${hostIP}:5174/add_user/${Username}`, { method: 'POST' })
         .then(res => {
-          setResponse({ status: res.status == 200 });
-          console.log(res.status);
+          setResponse(prev => ({ ...prev, color: res.status === 200 ? 'green' : 'red' }));
           return res.json();
         })
         .then(data => {
-          console.log(data);
-          setResponse({ ...response, message: data.message });
+          setResponse(prev => ({ ...prev, message: data.message }));
         })
         .catch(error => {
-          console.error('Error:', error);
-          setResponse({ ...response, message: error.message });
+          setResponse(prev => ({ ...prev, message: data.message }));
+        })
+        .finally(() => {
+          setShowBanner(true);
         });
     }
   };
+
+  console.log(response);
 
   return (
     <>
       <Header title="Users" />
       <div className="flex-col flex-grow p-3">
         <div>
+          {showBanner && (
+            <Notification
+              message={response.message}
+              showBanner={showBanner}
+              setShowBanner={setShowBanner}
+              color={response.color}
+            />
+          )}
           <Input label="Add User" placeholder="ex. Admin" data="" buttonLabel="Submit" method={AddUser} />
         </div>
       </div>
---
+++ Software/client/src/components/Notification.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+const Notification = ({ message, color, showBanner, setShowBanner }) => {
+  useEffect(() => {
+    if (showBanner) {
+      const timer = setTimeout(() => {
+        setShowBanner(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [showBanner, setShowBanner]);
+
+  return (
+    <>
+      {showBanner && (
+        <div className={`fixed top-0 left-0 right-0 m-3 p-4 rounded-lg bg-${color}-500 text-white z-20`}>{message}</div>
+      )}
+    </>
+  );
+};
+
+export default Notification;
