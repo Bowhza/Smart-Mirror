f6f7f9a9f1773a7961e0e0b48f840c5708bf61b1
Bowhza <bogtank2003@gmail.com>
2024-04-05 19:05:12 -0600

    Updated style and scroll behavior on reminders and news.

--- Software/client/src/components/Reminders.jsx
+++ Software/client/src/components/Reminders.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useRef, useState, forwardRef } from 'react';
 
-const Reminder = forwardRef(({ title, description, startDate, endDate, isFocused }, ref) => {
+const Reminder = forwardRef(({ index, title, description, startDate, endDate, isFocused }, ref) => {
   return (
     <div
       ref={ref}
       className={`flex flex-col py-2 px-3 rounded-md border-2 bg-zinc-800 ${isFocused ? 'border-blue-500' : 'border-neutral-700'}`}
     >
-      <p className="font-bold">{title}</p>
+      <div className="flex justify-between">
+        <p className="font-bold">{title}</p>
+        <p className="text-neutral-600 font-bold text-lg">{index + 1}</p>
+      </div>
       <p>{description}</p>
       <div className="flex justify-between">
         <p>Start: {startDate}</p>
@@ -41,7 +44,12 @@ export default function Reminders({ reminders }) {
     }
   }, []);
 
-  const displayedReminders = reminders.slice(focusedIndex, focusedIndex + 5);
+  let displayedReminders;
+  if (focusedIndex < 5) {
+    displayedReminders = reminders.slice(0, 5);
+  } else {
+    displayedReminders = reminders.slice(focusedIndex - 4, focusedIndex + 1);
+  }
 
   return (
     <div className="m-3 flex-col md:col-start-6 md:col-span-2 text-neutral-50">
@@ -50,17 +58,25 @@ export default function Reminders({ reminders }) {
         {displayedReminders.length === 0 ? (
           <p className="font-bold">No reminders to display.</p>
         ) : (
-          displayedReminders.map((reminder, index) => (
-            <Reminder
-              key={reminder.reminderID}
-              title={reminder.title}
-              description={reminder.description}
-              startDate={reminder.startDate}
-              endDate={reminder.endDate}
-              isFocused={index === 0}
-              ref={index === 0 ? firstReminderRef : null}
-            />
-          ))
+          displayedReminders.map((reminder, index) => {
+            // Calculate the relative index of the reminder
+            const relativeIndex = focusedIndex < 5 ? index : focusedIndex - 4 + index;
+            // Determine if the reminder is focused
+            const isFocused = relativeIndex === focusedIndex;
+
+            return (
+              <Reminder
+                key={reminder.reminderID}
+                index={relativeIndex}
+                title={reminder.title}
+                description={reminder.description}
+                startDate={reminder.startDate}
+                endDate={reminder.endDate}
+                isFocused={isFocused}
+                ref={relativeIndex === focusedIndex ? firstReminderRef : null}
+              />
+            );
+          })
         )}
       </div>
     </div>
