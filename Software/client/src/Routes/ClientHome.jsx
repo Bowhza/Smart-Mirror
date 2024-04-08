617b7ee3feee56c37cc1048911f4118838eb8934
Bowhza <bbohnat1@nait.ca>
2024-04-08 12:48:02 -0600

    Updated client gome page to show controls for the sensors.

--- Software/client/src/Routes/ClientHome.jsx
+++ Software/client/src/Routes/ClientHome.jsx
@@ -25,15 +25,61 @@ export default function ClientHome({ socket }) {
   return (
     <>
       <Header title="Home" />
-      <div className="flex-col flex-grow p-3">
-        <h2 className="text-xl">
-          Socket Status:{' '}
-          {isConnected ? (
-            <span className="text-green-500 font-bold">Connected</span>
-          ) : (
-            <span className="text-red-500 font-bold">Disconnected</span>
-          )}
-        </h2>
+      <div className="flex flex-col flex-grow p-3 gap-2">
+        <div className="py-2 px-3 rounded-md border-2 bg-neutral-100 drop-shadow-sm">
+          <h2 className="font-bold text-2xl">Connection</h2>
+          <p className="text-xl">
+            Socket Status:{' '}
+            {isConnected ? (
+              <span className="text-green-500 font-bold">Connected</span>
+            ) : (
+              <span className="text-red-500 font-bold">Disconnected</span>
+            )}
+          </p>
+        </div>
+
+        <div className="py-2 px-3 rounded-md border-2 bg-neutral-100 drop-shadow-sm">
+          <h2 className="font-bold text-2xl">Accelerometer Controls</h2>
+          <p>
+            <span className="font-bold">Double Tap</span> toggles the display ON/OFF.
+          </p>
+        </div>
+
+        <div className="py-2 px-3 rounded-md border-2 bg-neutral-100 drop-shadow-sm">
+          <h2 className="font-bold text-2xl">Ambient Light Sensor</h2>
+          <p>
+            <span className="font-bold">If Enabled</span> will automatically adjust the display brightness based on the
+            measured light (LUX) level.
+          </p>
+          <p>
+            <span className="font-bold">If Disabled</span> manual override for display brightness is available.
+          </p>
+        </div>
+
+        <div className="py-2 px-3 rounded-md border-2 bg-neutral-100 drop-shadow-sm">
+          <h2 className="font-bold text-2xl">Gesture Controls</h2>
+          <p>If the gesture sensor is enabled:</p>
+          <p>
+            <span className="font-bold">Wave</span> toggles the display ON/OFF.
+          </p>
+          <p>
+            <span className="font-bold">Swipe UP</span> scrolls up on the reminders.
+          </p>
+          <p>
+            <span className="font-bold">Swipe DOWN</span> scrolls down on the reminders.
+          </p>
+          <p>
+            <span className="font-bold">Swipe LEFT</span> toggles pause on the news feed.
+          </p>
+        </div>
+
+        <div className="py-2 px-3 rounded-md border-2 bg-neutral-100 drop-shadow-sm">
+          <h2 className="font-bold text-2xl">Proximity Sensor</h2>
+          <p>
+            <span className="font-bold">If Enabled</span> will turn on the display on detected presence. Will turn off
+            automatically if no presence is detected for 5 seconds.
+          </p>
+        </div>
       </div>
     </>
   );
